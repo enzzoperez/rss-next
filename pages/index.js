ae1a492d041e925e92b1562dff703f64f02a0507
Enzzo <enzzorp@gmail.com>
2018-05-04 16:06:41 -0300

    Updated refactor

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import fetch from 'isomorphic-unfetch'
 const parseString = require('xml2js').parseString;
+import { extractDescription, extractImage } from "../utils/utils";
 import Layout from "../components/layout";
 import ListNews from "../components/listNews";
 
@@ -10,24 +11,13 @@ export default class Index extends Component {
         
         return(
             list.rss.channel[0].item.map(({title, link, description, pubDate}, index)=>{
-                let imgSrc
-                let shortDescription = null
+                let imgSrc = extractImage(description[0])
+                let shortDescription = extractDescription(description[0])
                 let date = new Date(pubDate); 
-                parseString(description[0], (err, result) => {
-                    if(result){
-                        shortDescription = description[0].substring(description[0].indexOf('div>') + 4)
-                        imgSrc = result.div.img[0].$.src
-                    }
-                    else{ 
-                        imgSrc = 'http://www.industrialparts.com.sv/wp-content/uploads/2017/02/Error_l-531.jpg'
-                    }
-                });
                 return (
                     <div key={index}>
                         <img src={imgSrc}/>
-                        {shortDescription? 
-                        <div dangerouslySetInnerHTML={{__html: `Resumen: ${shortDescription}`}} />: 
-                        <div>Resumen no disponible</div>}  
+                        <div dangerouslySetInnerHTML={{__html: `Resumen: ${shortDescription}`}} />
                         <div dangerouslySetInnerHTML={{__html: `Titulo: ${title}`}} />
                         <p>Fecha: {date.toLocaleDateString()} - Hora: {date.toLocaleTimeString()}</p>
                         <hr></hr>
