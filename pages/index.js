4e41023d0e1e6a38b39b6e38ae331e04530b26f5
Enzzo <enzzorp@gmail.com>
2018-05-04 03:01:34 -0300

    updated list

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import fetch from 'isomorphic-unfetch'
-import html from 'react-inner-html';
 const parseString = require('xml2js').parseString;
 import Layout from "../components/layout";
 import ListNews from "../components/listNews";
 
 export default class Index extends Component {
-    // componentDidMount(){
-    //     this.renderList()
-    // }
     renderList () {
         const {list} = this.props
-        console.log('mira', list.rss.channel[0].item);
+        
         return(
-            list.rss.channel[0].item.map(({title, link, description}, index)=>{
+            list.rss.channel[0].item.map(({title, link, description, pubDate}, index)=>{
+                let imgSrc
+                let shortDescription = null
+                let date = new Date(pubDate); 
+                parseString(description[0], (err, result) => {
+                    if(result){
+                        shortDescription = description[0].substring(description[0].indexOf('div>') + 4)
+                        imgSrc = result.div.img[0].$.src
+                    }
+                    else{ 
+                        imgSrc = 'http://www.industrialparts.com.sv/wp-content/uploads/2017/02/Error_l-531.jpg'
+                    }
+                });
                 return (
                     <div key={index}>
-                        {<div dangerouslySetInnerHTML={{__html: description}} />}
+                        <img src={imgSrc}/>
+                        {shortDescription? 
+                        <div dangerouslySetInnerHTML={{__html: `Resumen: ${shortDescription}`}} />: 
+                        <div>Resumen no disponible</div>}  
                         <div dangerouslySetInnerHTML={{__html: `Titulo: ${title}`}} />
-                        <p>link: {link}</p>
+                        <p>Fecha: {date.toLocaleDateString()} - Hora: {date.toLocaleTimeString()}</p>
                         <hr></hr>
                     </div>
                 )
