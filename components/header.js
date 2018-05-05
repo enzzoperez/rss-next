e1430546fdedd8d5f21812a9b5ff3729845d8e3f
Enzzo <enzzorp@gmail.com>
2018-05-05 12:41:27 -0300

    update components

--- components/header.js
+++ components/header.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import Link from "next/link";
+
+import { ListPapers } from "../pages/listPapers";
+
+const sections = [
+	{section: 'Ultimas noticias', url:'/'},
+	{section: 'Lista de diarios', url:'/listPapers'},
+	{section: 'Categorias', url:'/aboutPage'},
+	{section: 'Clima', url:'/aboutPage'},
+	{section: 'Next Innovation Lab', url:'/aboutPage'}
+]
 
 export default class Header extends Component {
   render() {
     return (
-        <div>
-            Header
-        </div>
+		<div>
+			<ul>
+				{sections.map((item, index)=> {
+					return(
+						<li key={index}>
+							<Link href={item.url}>
+								<a>{item.section}</a>
+							</Link>
+						</li>
+					)
+				})}
+			</ul>
+		</div>
     )
   }
 };
