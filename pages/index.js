5256357f8d04d68933c587fa750022b00cd30f68
Enzzo <enzzorp@gmail.com>
2018-05-05 12:41:05 -0300

    update pages list papers and index

--- pages/index.js
+++ pages/index.js
@@ -6,31 +6,21 @@ import Layout from "../components/layout";
 import ListNews from "../components/listNews";
 
 export default class Index extends Component {
-    renderList () {
-        const {list} = this.props
-        
-        return(
-            list.rss.channel[0].item.map(({title, link, description, pubDate}, index)=>{
-                let imgSrc = extractImage(description[0])
-                let shortDescription = extractDescription(description[0])
-                let date = new Date(pubDate); 
-                return (
+    
+
+    render() {
+        const {news} = this.props        
+        return (
+            <Layout>
+                {news.map(({title, imgSrc, shortDescription, date, time}, index)=>(
                     <div key={index}>
                         <img src={imgSrc}/>
                         <div dangerouslySetInnerHTML={{__html: `Resumen: ${shortDescription}`}} />
                         <div dangerouslySetInnerHTML={{__html: `Titulo: ${title}`}} />
-                        <p>Fecha: {date.toLocaleDateString()} - Hora: {date.toLocaleTimeString()}</p>
+                        <p>Fecha: {date} - Hora: {time}</p>
                         <hr></hr>
                     </div>
-                )
-            })
-        )
-    }
-
-    render() {        
-        return (
-            <Layout>
-                {this.renderList()}
+                ))}
             </Layout>
         )
     }
@@ -40,8 +30,18 @@ Index.getInitialProps = async ({req}) => {
     const response = await fetch('http://www.elesquiu.com/rss/un_foto.html')
     const xml = await response.text()
     let list
+    let news = []
     parseString(xml, (err, result) => {
         list = result
     });
-    return {list: list}
+    news = list.rss.channel[0].item.map(({title, link, description, pubDate}, index)=>{
+        let titleProp = title
+        let imgSrc = extractImage(description[0])
+        let shortDescription = extractDescription(description[0])
+        let date = new Date(pubDate);
+        let dateNow = date.toLocaleDateString()
+        let time = date.toLocaleTimeString()
+        return {title:titleProp, imgSrc: imgSrc, shortDescription: shortDescription, date: dateNow, time: time}
+    })
+    return {news: news}
 }
--- pages/listPapers.js
+++ pages/listPapers.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import Link from "next/link";
+import Layout from "../components/layout";
+
+const papers = [
+	{nombre: 'El esquiu', url:'/ancasti'},
+	{nombre: 'El ancasti', url:'/ancasti'},
+	{nombre: 'Catamarca Actual', url:'/ancasti'},
+	{nombre: 'Clarin', url:'/ancasti'}
+]
 
 export default class ListPapers extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+	render() {
+		return (
+			<Layout>
+				{papers.map((item, index)=>(
+					<Link href={item.url}>
+						<a key={index}>{item.nombre}</a>
+					</Link>
+				))}
+			</Layout>
+		)
+	}
 };
