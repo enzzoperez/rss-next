ea1a9a7dfde5836d1953dbf77981027d6cb132b5
Enzzo <enzzorp@gmail.com>
2018-05-05 12:39:08 -0300

    add about file

---
+++ pages/aboutPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Layout from "../components/layout";
+
+export default class AboutPage extends Component {
+  render() {
+    return (
+      <Layout></Layout>
+    )
+  }
+};
