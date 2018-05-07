f7d664251e39a4077bb93d94f63344dfdc703978
Enzzo <enzzorp@gmail.com>
2018-05-06 23:38:10 -0300

    update components list news, refactored(?

--- components/listNews.js
+++ components/listNews.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
+import { extractDescription, extractImage } from "../utils/utils";
+
 
 export default class ListNews extends Component {
-    state = {
-        list: [],
-        loading: true,
-        error: false
-    }
-
-    fetchRss = async ()=>{
-        const response = fetch('http://www.elesquiu.com/rss/un_foto.html')
-        if (!response.ok) { throw Error(response.statusText) }
-        console.log('yay', response);
-        parseString(response, (err, result) => {
-            console.logir('xml',result);
-        });
-    }
-
     render() {
+        const {news} = this.props
         return (
-        <div>
-            List news
-        </div>
+            news.items.map(({title, link, description, thumbnail, pubDate}, index)=>{
+                let shortDescription = extractDescription(description)
+                let date = new Date(pubDate)
+                let dateNow = date.toLocaleDateString()
+                let time = date.toLocaleTimeString()
+                return (
+                    <div key={index}>
+                        <img src={thumbnail}/>
+                        <div dangerouslySetInnerHTML={{__html: `Resumen: ${shortDescription}`}} />
+                        <div dangerouslySetInnerHTML={{__html: `Titulo: ${title}`}} />
+                        <p>Fecha: {dateNow} - Hora: {time}</p>
+                        <a href={link} target='_blank' rel='noreferrer noopener nofollow'>Ver noticia</a>
+                        <hr></hr>
+                    </div>
+                )
+            })
         )
     }
 };
