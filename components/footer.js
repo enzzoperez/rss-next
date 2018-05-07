74bc20d220bf1a263069469dffb593026572e72a
Enzzo <enzzorp@gmail.com>
2018-05-06 23:52:07 -0300

    changes classname atrib

--- components/footer.js
+++ components/footer.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render() {
     return (
-      <footer class="footer">
-        <div class="container bg-black">
-          <span class="text-muted">Place sticky footer content here.</span>
+      <footer className="footer">
+        <div className="container bg-black">
+          <span className="text-muted">Place sticky footer content here.</span>
         </div>
       </footer>
     )
