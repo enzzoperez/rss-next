1fe68f5f6b9ef4c5ac4050279070f9d87976786b
Enzzo <enzzorp@gmail.com>
2018-05-07 21:24:11 -0300

    update page index

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import fetch from 'isomorphic-unfetch'
+
 import Layout from "../components/layout";
-import ListNews from "../components/listNews";
+import NewsEsquiu from "../components/newsEsquiu";
+import NewsClarin from "../components/newsClarin";
+import NewsCatActual from "../components/newsCatActual";
+import NewsNacion from "../components/newsNacion";
 
 export default class Index extends Component {
 
     render() {
-        const {news} = this.props                
+        const {newsEsquiu, newsClarin, newsCatActual, newsNacion} = this.props
+                     
         return (
             <Layout>
-                <ListNews news={news}/>
+                <NewsEsquiu news={newsEsquiu}/>
+                <NewsClarin news={newsClarin}/>
+                <NewsCatActual news={newsCatActual} />
+                <NewsNacion news={newsNacion}/>
             </Layout>
         )
     }
@@ -18,9 +26,28 @@ export default class Index extends Component {
 
 Index.getInitialProps = async ({req}) => {
     const apiKey = 'ae6ifsllsv5k53bwud5dwhchjlyqmrcwctcxcbta'
-    const url =`https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fwww.elesquiu.com%2Frss%2Fun_foto.html&api_key=${apiKey}&count=20`
-    const response = await fetch(url)
-    const news = await response.json()
+    const apiKeyNacion='79b0a6432abc49f1a441245a915f065b'
 
-    return {news}
+    const urlEsquiu =`https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fwww.elesquiu.com%2Frss%2Fun_foto.html&api_key=${apiKey}&count=5`
+    const responseEsquiu = await fetch(urlEsquiu)
+    const newsEsquiu = await responseEsquiu.json()
+
+    const urlClarin = `https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.clarin.com%2Frss%2Flo-ultimo%2F&api_key=${apiKey}&count=5`
+    const responseClarin = await fetch(urlClarin)
+    const newsClarin = await responseClarin.json()
+
+    const urlCAtActual = `https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fwww.catamarcactual.com.ar%2Frss%2Fun_foto.html&api_key=${apiKey}&count=5`
+    const responseCatActual = await fetch(urlCAtActual)
+    const newsCatActual = await responseCatActual.json()
+
+    const urlNacion = `https://newsapi.org/v2/everything?sources=la-nacion&apiKey=${apiKeyNacion}&pageSize=5`
+    const responseNacion = await fetch(urlNacion)
+    const newsNacion = await responseNacion.json()
+
+    return {
+        newsEsquiu: newsEsquiu, 
+        newsClarin: newsClarin,
+        newsCatActual: newsCatActual,
+        newsNacion: newsNacion,
+    }
 }
