a6da97dc33c63b0489dd8219ee0b7ceb1514ba71
EduClerici <educlerici@gmail.com>
2018-05-09 23:33:40 -0300

    categories templates, journals templates

--- pages/listPapers.js
+++ pages/listPapers.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
+import { Row, Col, Card, CardTitle, CardText, CardDeck, CardBody, CardImg, CardImgOverlay } from 'reactstrap';
 import Link from "next/link";
 import Layout from "../components/layout";
 
 const papers = [
 	{name: 'El esquiu', url:'/journal'},
 	{name: 'El ancasti', url:'/journal'},
-	{name: 'Catamarca Actual', url:'/journal'},
+	{name: 'Cat Actual', url:'/journal'},
 	{name: 'Clarin', url:'/journal'}
 ]
 
 const JournalComponent = (props) => {
 	const {name, url} = props
 	return (
-		<Link href={`${url}/?name=${name}`}>
-			<a>{name}</a>
-		</Link>
+		<Card>
+			<CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+			<CardBody>
+				<CardTitle>
+					<Link href={`${url}/?name=${name}`} width="50%">
+						<a>{name}</a>
+					</Link>
+				</CardTitle>
+			</CardBody>
+
+			<style jsx>{`
+				a { 
+					font-family: Roboto;
+					font-size: 2rem;
+					color: black;
+				}
+			`}</style>
+		</Card>
 	)
 };
 
@@ -22,9 +38,13 @@ export default class ListPapers extends Component {
 	render() {
 		return (
 			<Layout>
-				{papers.map((item, index)=>(
-					<JournalComponent key={index} name={item.name} url={item.url}/>
-				))}
+				<Row>	
+					{papers.map((item, index)=>(
+						<Col class="mx-auto my-4">
+							<JournalComponent key={index} name={item.name} url={item.url}/>
+						</Col>
+					))}
+				</Row>
 			</Layout>
 		)
 	}
