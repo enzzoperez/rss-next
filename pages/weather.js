33ac82be4074f1b51dcaebe1a731efb12885f8f3
Enzzo <enzzorp@gmail.com>
2018-05-14 20:36:10 -0300

    update weather page to show current weather

--- pages/weather.js
+++ pages/weather.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout';
 import { convertToC } from "../utils/utils";
 
 const WeatherList = (props) => {
-    const {list} = props
+    const {currentWeather, list} = props
     return (
         list.map((item, index)=>{
             let d = new Date(item.dt_txt)
@@ -25,10 +25,14 @@ const WeatherList = (props) => {
 
 export default class Weather extends Component {
     render() {
-        const {listWeather} = this.props
+        const {currentWeather, listWeather} = this.props
         return (
             <Layout>
-                <WeatherList list={listWeather}/>
+                <div>
+                    Temperatura actual: <b>{currentWeather}</b>
+                    <hr></hr>
+                </div>
+                <WeatherList currentWeather={currentWeather} list={listWeather}/>
             </Layout>
         )
     }
@@ -37,12 +41,17 @@ export default class Weather extends Component {
 Weather.getInitialProps = async () => {
     const cityId = '3837702'
     const userId ='9cb235761e24dfccc8112e7c3419efc1'
-    const url = `http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${userId}&units=metric`
-    const response = await fetch(url)
-    const json = await response.json()
-    
-    const listWeather = json.list
+    const urlExtend = `http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${userId}&units=metric`
+    const urlCurrent = `http://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${userId}&units=metric`
 
-    return {listWeather}
+    let response = await fetch(urlExtend)
+    const jsonExtend = await response.json()
+    response = await fetch(urlCurrent)
+    const jsonCurrent = await response.json()
+    
+    const currentWeather = jsonCurrent.main.temp
+    const listWeather = jsonExtend.list
+
+    return {currentWeather, listWeather}
 }
 
