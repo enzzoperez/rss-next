d197e7e420d7c66b9aa731ab460bf73231b98e4f
Enzzo <enzzorp@gmail.com>
2018-05-14 16:45:41 -0300

    changes in server

--- server.js
+++ server.js
@@ -1,24 +1,54 @@
+// const express = require('express')
+// const next = require('next')
+
+// const dev = process.env.NODE_ENV !== 'production'
+// const app = next({ dev })
+// const handle = app.getRequestHandler()
+
+// app.prepare()
+// .then(() => {
+//   const server = express()
+
+//   server.get('*', (req, res) => {
+//     return handle(req, res)
+//   })
+
+//   server.listen(3000, (err) => {
+//     if (err) throw err
+//     console.log('> Ready on http://localhost:3000')
+//   })
+// })
+// .catch((ex) => {
+//   console.error(ex.stack)
+//   process.exit(1)
+// })
+const { createServer } = require('http');
 const express = require('express')
-const next = require('next')
+const { parse } = require('url');
+const { createReadStream } = require('fs');
+const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-app.prepare()
-.then(() => {
-  const server = express()
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname } = parsedUrl;
+    const server = express()
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
-
-  server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+    if (pathname === '/sw.js') {
+      res.setHeader('content-type', 'text/javascript');
+      createReadStream('./offline/serviceWorker.js').pipe(res);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(3000, err => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
