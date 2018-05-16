aba94c446816abdc472a29580223b8761f8f4c7b
EduClerici <educlerici@gmail.com>
2018-05-16 11:54:17 -0300

    default imagen journals on error

--- components/newsEsquiu.js
+++ components/newsEsquiu.js
@@ -18,7 +18,7 @@ export default class NewsEsquiu extends Component {
                         <Col lg="3" xl="3"></Col>
                         <Col lg="6" xl="6">
                             <Card className="itemNews mb-3" href={link}>
-                                <CardImg onError={this.addDefaultSrc} style={{ width: "100%" }} src={thumbnail}/>
+                                <CardImg  style={{ width: "100%" }} src="static/img-default-elesquiu.png"/>
                                 <CardBody>
                                     <Badge className="mb-3" color="warning">
                                         El Esquiu
@@ -80,8 +80,5 @@ export default class NewsEsquiu extends Component {
             })
         )
         
-    }
-    addDefaultSrc(ev){
-        ev.target.src = 'static/img-default-elesquiu.png'
     }
 };
