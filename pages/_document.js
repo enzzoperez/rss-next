586cd19270b151a1eccb5e56d0e4e09c8b99c0cf
EduClerici <educlerici@gmail.com>
2018-05-15 22:03:11 -0300

    fixing audits failures

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default class MyDocument extends Document {
     return (
       <html lang="es">
         <Head>
+          <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
           <Main />
